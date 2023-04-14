4dea8841c0dd39716bb51029b584c544f484efcf
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-04-14 10:26:13 +0530

    Add files via upload

---
+++ DAY 4/CopyStr/CopyStr/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// sorting of a charecter array
+namespace SortChArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            char[] arr1;
+            char ch;
+            int i, j, l;
+            Console.Write("Input the string");
+            str = Console.ReadLine();
+            l = str.Length;
+            arr1 = str.ToCharArray(0, l);
+
+            for (i = 1; i < l; i++)
+                for (j = 0; j < l - i; j++)
+
+                    if (arr1[j] > arr1[j + 1])
+                    {
+                        ch = arr1[j];
+                        arr1[j] = arr1[j + 1];
+                        arr1[j + 1] = ch;
+                    }
+            Console.Write("After sorting the string \n");
+            foreach (char c in arr1)
+            {
+                ch = c;
+                Console.Write("{0} ", ch);
+            }
+        }
+            
+
+            }
+        }
+    
+
---
+++ DAY 4/CountWords/CountWords/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Count the total number of words in a string
+namespace CountWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int  word, l;
+            Console.WriteLine("Input the string ");
+            str = Console.ReadLine();
+
+            l = 0;
+            word = 1;
+            while (l <= str.Length - 1)
+            {
+                if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+                {
+                    word++;
+                }
+
+                l++;
+            }
+            Console.WriteLine("Total number of words in the string is  {0}", word);
+        }
+    }
+}

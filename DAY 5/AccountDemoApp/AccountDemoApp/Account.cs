51abe9ef85a99d89365d37bc784d7dba138466c0
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-04-14 10:27:14 +0530

    Add files via upload

---
+++ DAY 5/AccountDemoApp/AccountDemoApp/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AccountDemoApp
+{
+    public class Account
+    {
+        public int Id;
+        public string Name;
+        public decimal Balance;
+        
+        public void CreateAccount(int id, string name, decimal balance)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Balance = balance;
+        }
+
+            public void Deposite(decimal amount)
+            {
+                
+            Balance += amount;
+            }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Account Information");
+            Console.WriteLine("Account Id :"+Id);
+            Console.WriteLine("Name :"+Name);
+            Console.WriteLine("Balance : "+Balance);
+        }
+        public void withdraw(decimal amount)
+        {
+            if (Balance - amount < 1000)
+                Console.WriteLine("Insufficient Balance");
+            else
+                this.Balance = amount;
+        }
+    }
+}

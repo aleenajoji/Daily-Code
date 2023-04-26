69fc52e2a5c5105ce5cae1a9383740735bb63213
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-04-26 09:41:33 +0530

    Add files via upload

---
+++ DAY 16/MissinNO/MissinNO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Finding the missing element from array
+namespace MissinNO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0,total=0;
+            Console.WriteLine("Enter size");
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter array elemts");
+            int[] arr = new int[n];
+            for (int i = 0; i < n-1; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+            
+            total = (n * (n + 1)) / 2;
+            for (int i = 0; i < n ; i++) 
+                sum = sum + arr[i];
+            int missingElmt = total - sum;//taking difference between total number and sum of array
+            Console.WriteLine("Missing Element {0}",missingElmt);
+        }
+    }
+}

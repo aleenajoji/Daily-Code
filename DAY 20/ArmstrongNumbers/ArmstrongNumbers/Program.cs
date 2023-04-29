e9524b7e4fb29f4838519629b66f84270210655b
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-04-29 17:43:31 +0530

    Add files via upload

---
+++ DAY 20/ArmstrongNumbers/ArmstrongNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a three digit num");
+            int num = int.Parse(Console.ReadLine());
+           
+            isArmstrong(num);
+        }
+
+
+
+            static void isArmstrong(int num)
+            {
+            int a, digit = 0,temp;
+            temp = num;
+
+            while (temp > 0)
+                {
+                    a = temp % 10;
+                    digit = digit + (a * a * a);
+                    temp = temp / 10;
+                }
+                if (digit == num)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        
+    }
+}
---
+++ DAY 20/ConvertFiveProgram/ConvertFiveProgram/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class ConvertFiveProgram
+
+{
+    public static void Main()
+
+    {
+        Console.WriteLine("How much number of elements to be converted");
+        int T = int.Parse(Console.ReadLine());
+        for (int i = 0; i < T; i++)
+
+        {
+            Console.WriteLine("Enter the number\t");
+            int n = int.Parse(Console.ReadLine());
+            int converted_n = ConvertFive(n);
+            Console.WriteLine("After replacing 0's with 5 --> {0}",converted_n);
+
+        }
+
+    }
+
+    public static int ConvertFive(int n)
+
+    {
+        int result = 0;
+        int multiplier = 1;
+
+        while (n > 0)
+
+        {
+            int digit = n % 10;
+            if (digit == 0)
+
+            {
+                digit = 5;
+            }
+
+            result = digit * multiplier + result;
+            multiplier *= 10;
+            n /= 10;
+
+        }
+        return result;
+
+    }
+
+}
+

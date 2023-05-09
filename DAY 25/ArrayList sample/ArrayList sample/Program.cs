731dcf04d847edac0014b8c59c2a3d4b7906e7bc
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-05-09 08:50:02 +0530

    Add files via upload

---
+++ DAY 25/ArrayList sample/ArrayList sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrlis = new ArrayList()
+            {
+                1,"Bill","doooo",10000
+
+            };
+            foreach(var i in arrlis)
+            {
+                Console.WriteLine(i);
+            }
+            foreach(var i in arrlis.GetRange(1,3))
+            {
+                Console.WriteLine(  i);
+            }
+
+        }
+    }
+}
---
+++ DAY 25/MigrationinEF/MigrationinEF/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationinEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class SchoolDBContext : DbContext
+    {
+        public SchoolDBContext() : base("SchoolDBConnectionString")
+        {
+            Database.SetInitializer<SchoolDBContext>(new CreateDatabaseIfNotExists<SchoolDBContext>());
+
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseIfModelChanges<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseAlways<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new SchoolDBInitializer());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+        public object Database { get; }
+    }
+    public class SchoolDBInitializer : CreateDatabaseIfNotExists<SchoolDBContext>
+    {
+        protected override void Seed(SchoolDBContext context)
+        {
+            base.Seed(context);
+        }
+    }
+
+}

2f37908d4fe6446077c838d192836b28d20d8800
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-05-26 11:32:34 +0530

    Add files via upload

---
+++ DAY 42/AnagramsQ2/AnagramsQ2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Anagram
+
+{ public class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter String 1: ");
+
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter String 2: ");
+
+            string str2 = Console.ReadLine();
+
+            if (Anagram(str1, str2))
+
+            {
+
+                Console.WriteLine("Anagram");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Not Anagram");
+
+            }
+
+        }
+
+        public static bool Anagram(string str1, string str2)
+
+        {
+
+            str1 = str1.ToLower();
+
+            str2 = str2.ToLower();
+
+            if (str1.Length != str2.Length)
+
+            {
+
+                return false;
+
+            }
+
+            int[] chrfrq = new int[26];
+
+            foreach (char c in str1)
+
+            {
+
+                chrfrq[c - 'a']++;
+
+            }
+
+            foreach (char c in str2)
+
+            {
+
+                if (chrfrq[c - 'a'] == 0)
+
+                {
+
+                    return false;
+
+                }
+
+                chrfrq[c - 'a']--;
+
+            }
+
+            return true;
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DAY 42/abstractclass/abstractclass/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+abstract class Book
+
+{
+    protected string title;
+    public abstract void SetTitle(string s);
+    public string GetTitle()
+
+    {
+
+        return title;
+
+    }
+
+}
+
+class MyBook : Book
+
+{
+
+    public override void SetTitle(string s)
+
+    {
+
+        title = s;
+
+    }
+
+}
+
+class MainClass
+
+{
+    public static void Main(string[] args)
+
+    {
+
+        MyBook myBook = new MyBook();
+
+        myBook.SetTitle("A tale of two cities");
+
+        Console.WriteLine("The title is: " + myBook.GetTitle());
+
+    }
+
+}
+
+
+
+
+
+
+
+
+

93feab56c3dd320fff7c4ff32884cbdc0aa59c51
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-05-29 09:18:41 +0530

    Add files via upload

---
+++ DAY 43/CodeChllg2/CodeChllg2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Result
+{
+    public static long Solve(List<int> arr)
+
+    {
+        Dictionary<int, long> frequency = new Dictionary<int, long>();
+        long count = 0;
+        foreach (int num in arr)
+        {
+            if (frequency.ContainsKey(num))
+                frequency[num]++;
+            else
+                frequency[num] = 1;
+        }
+        foreach (var kvp in frequency)
+        {
+            long freq = kvp.Value;
+            count += (freq * (freq - 1)) / 2;
+
+        }
+
+        return count;
+
+    }
+
+}
+
+class Solution
+
+{
+    static void Main(string[] args)
+
+    {
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+        List<int> arr = new List<int>(Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse));
+       long result = Result.Solve(arr);
+       Console.WriteLine(result);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ DAY 43/CodeChllg3/CodeChllg3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Result
+
+{
+    public static int birthdayCakeCandles(List<int> candles)
+
+    {
+        int maxHeight = 0;
+        int count = 0;
+        foreach (int height in candles)
+
+        {
+           if (height > maxHeight)
+            {
+                maxHeight = height;
+                count = 1;
+            }
+
+            else if (height == maxHeight)
+            {
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+
+}
+
+class Solution
+
+{
+    public static void Main(string[] args)
+
+    {
+       int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+        int result = Result.birthdayCakeCandles(candles);
+        Console.WriteLine(result);
+
+    }
+
+}
+
+
+
+
+

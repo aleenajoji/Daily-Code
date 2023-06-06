65c421fc295668b2d27016563f9af7ba96c2537c
aleenajoji <129928608+aleenajoji@users.noreply.github.com>
2023-06-06 14:29:46 +0530

    Add files via upload

---
+++ DAY 50/GenericsQ2/GenericsQ2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        int[] intArray = { 1, 2, 3 };
+        string[] stringArray = { "Hello", "World" };
+        PrintArray(intArray);
+        PrintArray(stringArray);
+    }
+
+    static void PrintArray<T>(T[] array)
+    {
+        foreach (T element in array)
+        {
+            Console.WriteLine(element);
+        }
+
+    }
+
+}
+
+
---
+++ DAY 50/HashSetQ3/HashSetQ3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+
+{
+   static void Main(string[] args)
+
+    {
+        int n = int.Parse(Console.ReadLine());
+        HashSet<string> pairs = new HashSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] pair = Console.ReadLine().Split(' ');
+            string pairString = $"{pair[0]},{pair[1]}";
+            pairs.Add(pairString);
+            Console.WriteLine(pairs.Count);
+        }
+    }
+}
